58faa04548134c495ff4b9cf2d97445fb447dd9c
Quan Gian <82435818+thq4n@users.noreply.github.com>
2025-05-19 16:08:38 +0700

    feat(font-size): enhance font size structure with new implementations
    - Updated file references to align with the new naming convention for font size components.
    - Introduced new classes for various text sizes (xs, sm, base, lg, xl, xxl, xxxl, xxxxl, xxxxxl) implementing the DSFontSize abstract class.
    - Added a new abstract class for font sizes and its implementation to streamline font size management within the design system.

--- lib/design_system_core/ds_font_size/ds_font_size_core.dart
+++ lib/design_system_core/ds_font_size/ds_font_size_core.dart
@@ -1,14 +1,14 @@
-part 'size/font_size.dart';
-part 'sizes/font_sizes.dart';
-part 'sizes/impl/font_sizes.impl.dart';
+part 'size/ds_font_size.dart';
+part 'sizes/ds_font_sizes.dart';
+part 'sizes/impl/ds_font_sizes.impl.dart';
 
 // Text sizes from smallest to largest
-part 'size/impl/text_xs.impl.dart';
-part 'size/impl/text_sm.impl.dart';
-part 'size/impl/text_base.impl.dart';
-part 'size/impl/text_lg.impl.dart';
-part 'size/impl/text_xl.impl.dart';
-part 'size/impl/text_xxl.impl.dart';
-part 'size/impl/text_xxxl.impl.dart';
-part 'size/impl/text_xxxxl.impl.dart';
-part 'size/impl/text_xxxxxl.impl.dart';
+part 'size/impl/ds_text_xs.impl.dart';
+part 'size/impl/ds_text_sm.impl.dart';
+part 'size/impl/ds_text_base.impl.dart';
+part 'size/impl/ds_text_lg.impl.dart';
+part 'size/impl/ds_text_xl.impl.dart';
+part 'size/impl/ds_text_xxl.impl.dart';
+part 'size/impl/ds_text_xxxl.impl.dart';
+part 'size/impl/ds_text_xxxxl.impl.dart';
+part 'size/impl/ds_text_xxxxxl.impl.dart';
