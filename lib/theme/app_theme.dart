ae8d53ce43a382831ca51eef6f29930f555c94ca
Quan Gian <82435818+thq4n@users.noreply.github.com>
2025-06-03 13:39:27 +0700

    feat(fonts): add SF Pro Display font family and update pubspec configuration
    - Introduced the SF Pro Display font family with various styles (Black, Bold, Heavy, Light, Medium, Regular, Semibold, Thin, Ultralight) to enhance typography options.
    - Updated pubspec.yaml to include flutter_gen for asset management and configured font paths for better organization.
    - Adjusted app theme to utilize the new font family for improved design consistency.

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,12 +1,14 @@
 // lib/theme/app_theme.dart
 import 'package:flutter/material.dart';
 import '../design_system_core/typography.dart';
+import '../gen/fonts.gen.dart';
 
 class AppTheme {
   static ThemeData get lightTheme {
     return ThemeData(
       // primaryColor: DesignColors.primaryColor,
       textTheme: TypographyStyles.lightTextTheme,
+      fontFamily: Fonts.sFProDisplay,
     );
   }
 }
