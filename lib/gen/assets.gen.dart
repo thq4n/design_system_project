afb9940e1383866c2ed823b9250b2aa01ab472d9
Quan Gian <82435818+thq4n@users.noreply.github.com>
2025-06-03 13:39:40 +0700

    feat(empty-state-assets): add new empty state icons and asset generation
    - Introduced new empty state icons (error, ok, question, search) in multiple resolutions (1x, 2x, 3x) to enhance user experience.
    - Generated asset management code in assets.gen.dart for easy access to the new icons within the application.

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,114 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+
+class $AssetsEmptyStateGen {
+  const $AssetsEmptyStateGen();
+
+  /// File path: assets/empty_state/icon_empty_state_error.png
+  AssetGenImage get iconEmptyStateError =>
+      const AssetGenImage('assets/empty_state/icon_empty_state_error.png');
+
+  /// File path: assets/empty_state/icon_empty_state_ok.png
+  AssetGenImage get iconEmptyStateOk =>
+      const AssetGenImage('assets/empty_state/icon_empty_state_ok.png');
+
+  /// File path: assets/empty_state/icon_empty_state_question.png
+  AssetGenImage get iconEmptyStateQuestion =>
+      const AssetGenImage('assets/empty_state/icon_empty_state_question.png');
+
+  /// File path: assets/empty_state/icon_empty_state_search.png
+  AssetGenImage get iconEmptyStateSearch =>
+      const AssetGenImage('assets/empty_state/icon_empty_state_search.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+    iconEmptyStateError,
+    iconEmptyStateOk,
+    iconEmptyStateQuestion,
+    iconEmptyStateSearch,
+  ];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetsEmptyStateGen emptyState = $AssetsEmptyStateGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName, {this.size, this.flavors = const {}});
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.medium,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({AssetBundle? bundle, String? package}) {
+    return AssetImage(_assetName, bundle: bundle, package: package);
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
