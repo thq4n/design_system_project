28ef74dd0fdf7ac19ace6f21bebb5cd0c0550bfa
Quan Gian <82435818+thq4n@users.noreply.github.com>
2025-06-18 16:06:45 +0700

    refactor(icon-constants): remove unused size and type constants
    - Deleted the size_constants.dart and type_constants.dart files as they were no longer in use.
    - Updated ds_icon_core.dart to reflect the removal of these constants for improved code maintainability.

--- lib/design_system_core/ds_icon_by_font/ds_icon_core.dart
+++ lib/design_system_core/ds_icon_by_font/ds_icon_core.dart
@@ -11,6 +11,3 @@ part 'base/impl/ds_icon_linear.impl.dart';
 
 part 'usage/ds_icons.dart';
 part 'usage/impl/ds_icons.impl.dart';
-
-part 'constants/size_constants.dart';
-part 'constants/type_constants.dart';
