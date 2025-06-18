85d6e650854b1f025ad8fa94416da67752824c7e
Quan Gian <82435818+thq4n@users.noreply.github.com>
2025-06-18 16:08:07 +0700

    feat(extensions): add extensions for state and SVG handling
    - Introduced a new extensions.dart file to manage state and SVG extensions.
    - Created state.extension.dart to define a StateExtension for enhanced state management capabilities.
    - Improved organization of extension functionalities for better code maintainability.

---
+++ lib/extensions/extensions.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart' as _svg;
+
+import '../design_system_project.dart';
+import '../gen/assets.gen.dart';
+
+part 'state.extension.dart';
+part 'svg.extension.dart';
---
+++ lib/extensions/state.extension.dart
@@ -0,0 +1,5 @@
+part of 'extensions.dart';
+
+extension StateExtension on State {
+  // DSTextTheme get textTheme => Theme.of(context).textTheme;
+}
