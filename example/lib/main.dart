4b4bf452f0dabb3d70220c1b69ed57091045cba9
Quan Gian <82435818+thq4n@users.noreply.github.com>
2025-11-26 10:02:13 +0700

    chore(main): import DSTooltip component for usage in main application

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:design_system_project/components/ds_tooltip/ds_tooltip.dart';
 import 'package:design_system_project/design_system_project.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
