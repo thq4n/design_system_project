43856998fb015faa782edcbafa6170532ee3d8f7
Quan Gian <quan.gian.thieu@247express.vn>
2026-08-25 09:08:27 +0700

    feat(state): add buildEmptyState method for handling empty states
    - Introduced buildEmptyState method in StateExtension to display a customizable empty state widget.
    - Supports optional message, subMessage, padding, and image size parameters for flexibility.

--- lib/extensions/state.extension.dart
+++ lib/extensions/state.extension.dart
@@ -3,4 +3,45 @@ part of 'extensions.dart';
 extension StateExtension on State {
   DSTextTheme get textTheme => context.textTheme;
   DSColors get colors => context.colors;
+
+   Widget buildEmptyState({
+    String? message = 'Không có dữ liệu',
+    String? subMessage,
+    EdgeInsetsGeometry padding = const EdgeInsets.all(40),
+    double imageSize = 200.0,
+  }) {
+    return Padding(
+      padding: padding,
+      child: ListView(
+        padding: EdgeInsets.zero,
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        children: [
+          DSImageView(
+            source: DSAssets.emptyState.iconEmptyStateQuestion,
+            width: imageSize,
+            height: imageSize,
+          ),
+          if (message != null) ...[
+            const SizedBox(height: 16),
+            Text(
+              message,
+              style: textTheme.lg?.semibold,
+              textAlign: TextAlign.center,
+            ),
+          ],
+          if (subMessage != null)
+            ...[
+              Text(
+                subMessage,
+                style: textTheme.base?.regular.copyWithColor(
+                  DSColorUsages.text.tertiary,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ].withSeparators(separator: const SizedBox(height: 8)),
+        ],
+      ),
+    );
+  }
 }
